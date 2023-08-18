75a6dc3c44aad794498b23783e3cc87573ca5968
prasadsawant7 <prasadsawant7git@gmail.com>
2023-08-18 14:11:05 +0530

    some minor changes

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,19 @@
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
+import torch
+import emoji
 import pickle
+# import pickle
 
-with open("./model/sentiment_analysis_pretrained.pkl", "rb") as model_file:
-    sa_model = pickle.load(model_file)
+cuda_available = torch.cuda.is_available()
+
+sa_model = ClassificationModel(
+    model_type="bertweet",
+    model_name="finiteautomata/bertweet-base-sentiment-analysis",
+    use_cuda=cuda_available
+)
+
+# with open("./model/sentiment_analysis_pretrained.pkl", "rb") as model_file:
+#     sa_model = pickle.load(model_file)
 
 def get_sentiment(text: str) -> list:
     prediction, _ = sa_model.predict([text])
